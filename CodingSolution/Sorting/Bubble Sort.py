8384ed5ef94fdd458e5dad1a3f847c8caf92545d
alif2499 <alifjcc99@gmail.com>
2025-03-08 16:34:41 -0700

    solved median of two sorted arrays

--- CodingSolution/Sorting/Bubble Sort.py
+++ CodingSolution/Sorting/Bubble Sort.py
@@ -1,11 +1,13 @@
 def bubbleSort(array):
+    counter = 0
     while True:
         isSwap = False
-        for i in range(len(array) - 1):
+        for i in range(len(array) - 1 - counter):
             if array[i] <= array[i + 1]:
                 continue
             else:
                 array[i], array[i + 1] = array[i + 1], array[i]
                 isSwap = True
+        counter += 1
         if not isSwap:
             return array
