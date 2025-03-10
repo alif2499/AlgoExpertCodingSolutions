f82bde842ef89404dcb443691561895f7021841a
alif2499 <alifjcc99@gmail.com>
2025-03-09 23:32:14 -0600

    solved insertion sort

---
+++ CodingSolution/Sorting/Insertion Sort.py
@@ -0,0 +1,14 @@
+def insertionSort(array):
+    for i in range(1, len(array)):
+        step = 1
+        while i - step >= 0:
+            if array[i] < array[i - step]:
+                swap(array, i, i - step)
+                i -= 1
+            else:
+                step += 1
+    return array
+
+def swap(array, i, j):
+    array[i], array[j] = array[j], array[i]
+    return array
