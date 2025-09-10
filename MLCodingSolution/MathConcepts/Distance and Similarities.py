cdad74f3c9149cf3b48e20c3cfc9e85b87eb7631
alif2499 <alifjcc99@gmail.com>
2025-09-10 13:42:31 -0600

    Solved Distance and Similarities

---
+++ MLCodingSolution/MathConcepts/Distance and Similarities.py
@@ -0,0 +1,27 @@
+from math import sqrt, pow
+class Metrics():
+   def euclidean_distance(self, X, Y):
+       return sqrt(sum((pow(x-y, 2)) for x, y in zip(X, Y)))
+
+   def manhattan_distance(self, X, Y):
+       return sum((abs(x-y)) for x, y in zip(X, Y))
+
+   def cosine_similarity(self, X, Y):
+       numerator = sum((x*y) for x, y in zip(X, Y))
+       denominator = sqrt(sum([x*x for x in X])) * sqrt(sum([y*y for y in Y]))
+
+       return numerator/float(denominator)
+
+   def jaccard_similarity(self, X, Y):
+       intersection_cardinality = len(set.intersection(*[set(X), set(Y)]))
+       union_cardinality = len(set.union(*[set(X), set(Y)]))
+
+       return intersection_cardinality/float(union_cardinality)
+
+
+def distances_and_similarities(X, Y):
+   metrics = Metrics()
+   return [metrics.euclidean_distance(X, Y),
+           metrics.manhattan_distance(X, Y),
+           metrics.cosine_similarity(X, Y),
+           metrics.jaccard_similarity(X, Y)]
